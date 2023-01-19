0f0ffa65fc456a46c113b21a9cf379c42ab78680
nikos efthias <nikos@mugsoft.io>
2023-01-20 01:56:43 +0300

    nicer dicer

--- src/main.rs
+++ src/main.rs
@@ -30,14 +30,61 @@ window.sign_form.onsubmit = function(e) {
                 let input_html = input_html.to_string();
                 async move {
                     let callsign = r.param("callsign").unwrap();
-                    let name = get_name(callsign).await.unwrap();
+                    let (name, dmr) = tokio::join!(get_name(callsign), get_dmr_data(callsign));
+                    let (name, dmr) = (
+                        name.unwrap_or_default(),
+                        dmr.unwrap_or_default().unwrap_or_default(),
+                    );
                     Ok(Response::builder()
                         .header(CONTENT_TYPE, "text/html; charset=UTF-8")
-                        .body(if name.is_empty() {
-                            format!("{} için kayıt bulunamadı.{input_html}", callsign)
-                        } else {
-                            format!("{} - {}{input_html}", callsign, name)
-                        })
+                        .body::<String>(format!(
+                            r#"
+                            <style>
+                            table, th, td {{
+                                border: 1px solid black;
+                                border-collapse: collapse;
+                            }}
+                            table{{
+                            width: 100%;
+                            text-align: center;
+                            margin:auto auto;
+                            }}
+                            </style>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>çağrı işareti</td>
+                                        <th>isim(callbook)</td>
+                                        <th>isim(dmr)</td>
+                                        <th>şehir</td>
+                                        <th>ülke</td>
+                                        <th>dmrid</td>
+                                        <th>bölge</td>
+                                    </tr>
+                                </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{callsign}</td>
+                                    <td>{name}</td>
+                                    <td>{dmr_fname} {dmr_surname}</td>
+                                    <td>{dmr_city}</td>
+                                    <td>{dmr_country}</td>
+                                    <td>{dmr_id}</td>
+                                    <td>{dmr_state}</td>
+                                </tr>
+                            </tbody>
+                            </table>
+                            {input_html}
+                            "#,
+                            callsign = callsign,
+                            name = name,
+                            dmr_fname = dmr.fname,
+                            dmr_surname = dmr.surname,
+                            dmr_city = dmr.city,
+                            dmr_country = dmr.country,
+                            dmr_id = dmr.id,
+                            dmr_state = dmr.state
+                        ))
                         .unwrap())
                 }
             })
@@ -73,7 +120,38 @@ window.sign_form.onsubmit = function(e) {
     )
     .await;
 }
-async fn get_name(callsign: &str) -> Result<String, Box<dyn Error>> {
+#[derive(Clone, Debug, serde::Deserialize, Default)]
+struct DmrData {
+    #[serde(default)]
+    pub fname: String,
+    #[serde(default)]
+    pub surname: String,
+    #[serde(default)]
+    pub city: String,
+    #[serde(default)]
+    pub country: String,
+    #[serde(default)]
+    pub id: usize,
+    #[serde(default)]
+    pub state: String,
+}
+async fn get_dmr_data(
+    callsign: &str,
+) -> Result<Option<DmrData>, Box<dyn Error + 'static + Send + Sync>> {
+    #[derive(Debug, serde::Deserialize)]
+    struct Response {
+        results: Vec<DmrData>,
+    }
+    let url = format!("https://radioid.net/api/dmr/user/?callsign={callsign}");
+    let body = reqwest::Client::new()
+        .get(&url)
+        .send()
+        .await?
+        .json::<Response>()
+        .await?;
+    Ok(body.results.get(0).cloned())
+}
+async fn get_name(callsign: &str) -> Result<String, Box<dyn Error + 'static + Send + Sync>> {
     //curl 'http://www.tacallbook.org/cgi-bin/bul1.cgi?ara=TA3KRT' \
     //-H 'Referer: http://www.tacallbook.org/call.shtml' > out.html
     let url = format!(
